6eae259d4ddea31e2c013d2bebe643fbecdb4b70
nassir <daniya_n@yahoo.com>
2021-04-22 18:12:30 -0700

    second-commit

--- deletepeople/index.js
+++ deletepeople/index.js
@@ -1,26 +1,31 @@
- 'use strict';
-
-
+'use strict';
 const peopleModel = require('./people.schema.js');
 const dynamoose = require('dynamoose')
 // const {v4:uuid} = require('uuid')
-exports.handler = async(event) =>{
-  let data ; 
+exports.handler = async (event) => {
+  let data;
   let id = event.pathParameters && event.pathParameters.id
-  
-    try {
-      data = await peopleModel.query('id').eq(id).exec();
-      let record = new peopleModel({id})
-     data = await record.delete()
-    }catch(e){
-      return{
-        statusCode :500,
-        body: e.message
-      }
+
+  try {
+    let result = await peopleModel.delete({ id: id })
+    console.log(result)
+  } catch (e) {
+
+    return {
+      statusCode: 500,
+      response: e.message,
     }
-    let response ={
-      statusCode:200,
-      body:JSON.stringify(data)
-    }
-    return response
   }
+
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  };
+
+
+  console.log(response.body)
+  return response;
+
+
+}
+
