c97d0bc9a915b7d1fc7cecbe0132d92a1216a1e8
nassir <daniya_n@yahoo.com>
2021-04-24 00:08:17 -0700

    final

--- deletepeople/index.js
+++ deletepeople/index.js
@@ -1,26 +1,69 @@
-'use strict';
-const dynamoose = require('dynamoose');
+// 'use strict';
+// const dynamoose = require('dynamoose');
+// const peopleModel = require('./people.schema.js');
+
+
+// exports.handler = async (event) => {
+//   console.log("EVENT===", event);
+
+
+//   try {
+//     const id = event.pathParameters && event.pathParameters.id;
+//     record = await peopleModel.query("id").eq(id).exec();
+//     const record = new peopleModel({id});
+
+//     data = await record.delete();
+
+//     return {
+//       statusCode: 201,
+//       body: JSON.stringify(data)
+//     }
+
+
+
+//   } catch (e) {
+//     return {
+//       statusCode: 500,
+//       body: e.message
+//     }
+//   }
+// }
+
+'use strict'
+
+
 const peopleModel = require('./people.schema.js');
+const dynamoose = require('dynamoose')
+const { v4: uuid } = require('uuid')
 
-exports.handler = async(event) => {
-  console.log('event======',event)
+exports.handler = async (event) => {
+  try {
+    let data;
+    const id = event.pathParameters && event.pathParameters.id;
 
-  let data;
-  
-  try{
-  const id = event.id
-    await peopleModel.delete({"id": id});
-    
-  }catch(e){
+
+    if (id) {
+      const result = await peopleModel.query('id').eq(id).exec();
+      data = result[0];
+    } else {
+      data = await peopleModel.scan().exec();
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+
+  } catch (e) {
     return {
       statusCode: 500,
-      body: e.message,
-    }
+      response: e.message,
+    };
   }
-  let response = {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  }
-  console.log(response.body);
-  return response;
-}
+
+};
+
+
+
+
+
+
