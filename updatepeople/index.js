257a5f144aeded4473532ff9a8c1915a3d0c2d5b
nassir <daniya_n@yahoo.com>
2021-04-23 19:24:22 -0700

    final

---
+++ updatepeople/index.js
@@ -0,0 +1,33 @@
+
+'use strict'
+
+
+const peopleModel = require('./people.schema.js');
+const dynamoose = require('dynamoose')
+const { v4: uuid } = require('uuid')
+
+exports.handler = async (event) => {
+  try {
+    let data;
+    const id = event.pathParameters && event.pathParameters.id;
+
+
+    if (id) {
+      const result = await peopleModel.query('id').eq(id).exec();
+      data = result[0];
+    } else {
+      data = await peopleModel.scan().exec();
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+
+  } catch (e) {
+    return {
+      statusCode: 500,
+      response: e.message,
+    };
+  }
+
+};
